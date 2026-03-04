8481eee10740b935399d7a8698b09ac98e11e6e6
ikk <2814634520@qq.com>
2026-03-04 15:09:31 +0800

    Implement factorial function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 // TODO: 实现两数相加函数 add
-// TODO: 实现阶乘函数 factorial
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 
 int main() {
     return 0;
